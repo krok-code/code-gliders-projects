801ba930020850adf261ce3ff4dbd53b2381532c
krok-code <maks.smolinskyi@gmail.com>
2023-12-09 19:26:55 +0200

    Update header.js

--- src/js/header.js
+++
@@ -1,9 +0,0 @@
-import localStorageAPI from './localStorage.js';
-
-export function getLength() {
-  const arrFromLS = localStorageAPI.load('product');
-  document.querySelector('#header-length').innerHTML = `${
-    arrFromLS ? arrFromLS.length : '0'
-  }`;
-}
-getLength();
