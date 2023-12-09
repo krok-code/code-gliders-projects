3ddbe0423501f22ce434b3b5c5b7029d62fcad96
krok-code <maks.smolinskyi@gmail.com>
2023-12-09 14:14:43 +0200

    updated two js files

--- src/js/api.js
+++ src/js/api.js
@@ -1,3 +1,24 @@
 import axios from 'axios';
+<<<<<<< HEAD
 import {getFilter} from './filters.js';
 const BASE_URL = 'https://food-boutique.b.goit.study/api/products';
+
+export async function getCategories() {
+	const response = await axios.get(`https://food-boutique.b.goit.study/api/products/categories`);
+	return response.data;
+}
+=======
+// import { getFilter } from './filters.js';
+const BASE_URL = 'https://food-boutique.b.goit.study/api/products';
+
+export const fetchPopularProducts = () => {
+  const params = {
+    limit: 5,
+  };
+  return axios(`${BASE_URL}/popular`, { params });
+};
+
+export const fetchDiscountProducts = () => {
+  return axios(`${BASE_URL}/discount`);
+};
+>>>>>>> 5a9bcaa803892034f3227ed681c212a6366cefff
---
+++ src/js/popular-discount.js
@@ -0,0 +1,103 @@
+import { fetchDiscountProducts, fetchPopularProducts } from './api';
+
+const refs = {
+  popular: document.querySelector('.popular_list'),
+  discount: document.querySelector('.discount_list'),
+};
+
+document.addEventListener('DOMContentLoaded', fetchPopular);
+document.addEventListener('DOMContentLoaded', fetchDiscount);
+
+async function fetchPopular() {
+  try {
+    const { data } = await fetchPopularProducts();
+    refs.popular.insertAdjacentHTML('beforeend', createPopularList(data));
+
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createPopularList(array) {
+  return array
+    .map(
+      card => ` <li class="card_item">
+      <div class="card_img">
+        <img
+          src=${card.img}
+          width="56"
+          height="56"
+          alt=${card.name}
+        />
+      </div>
+      <div class="card_elements">
+        <h3 class="card_title">${card.name}</h3>
+        <p class="card_text card_text--margin_b">
+          <span class="card_el">Category:</span>&nbsp;<span>${card.category}</span>
+        </p>
+        <p class="card_text">
+          <span class="card_el">Size:</span>&nbsp;<span>${card.size}</span
+          ><span class="card_el card_el--margin_l">Popularity:</span>&nbsp;<span
+            >${card.popularity}</span
+          >
+        </p>
+      </div>
+      <button type="button" class="card_btn">
+        <svg class="order_btn" width="12" height="12">
+          <use href="../img/icons.svg#shopping-cart"></use>
+        </svg>
+      </button>
+    </li>`
+    )
+    .join('');
+}
+
+async function fetchDiscount() {
+  try {
+    const { data } = await fetchDiscountProducts();
+    refs.discount.insertAdjacentHTML(
+      'beforeend',
+      createDiscountList(data.slice(0, 2))
+    );
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createDiscountList(array) {
+  return array
+    .map(
+      card => `
+    <li class="discount_card_item">
+      <div class="discount_card_img">
+        <img
+          src=${card.img}
+          width="114"
+          height="114"
+          alt=${card.name}
+        />
+        </div>
+        <div class="svg_box">
+          <svg width="60" height="60">
+            <use href="../img/icons.svg#discount">
+            </use>
+          </svg>
+        </div>
+
+      <div class="discount_card_elements">
+        <h3 class="card_title--discount card_title">${card.name}</h3>
+        <span class="card_title--price card_title ">${card.price}</span>
+
+        <button type="button" class="discount_card_btn">
+          <svg  width="18" height="18">
+          <use href="../img/icons.svg#shopping-cart"></use>
+          </svg>
+        </button>
+          
+      </div>  
+    </li> `
+    )
+    .join('');
+}
