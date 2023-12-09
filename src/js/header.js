87520d8ad8eaccb6617956cc1ed5050148d9e58e
Sad0328 <tours@soledad.com.ua>
2023-12-09 19:04:06 +0200

    1111

--- src/js/header.js
+++ src/js/header.js
@@ -2,8 +2,6 @@ import localStorageAPI from './local-storage.js';
 
 export function getLength() {
   const arrFromLS = localStorageAPI.load('product');
-  document.querySelector('#header-length').innerHTML = `${
-    arrFromLS ? arrFromLS.length : '0'
-  }`;
+  document.querySelector('#header-length').innerHTML = `${arrFromLS ? arrFromLS.length : '0'}`;
 }
 getLength();
