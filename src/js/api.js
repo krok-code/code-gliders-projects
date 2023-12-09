57ec1bff9aa9d076e21460a096f96bd964d8ab29
krok-code <maks.smolinskyi@gmail.com>
2023-12-09 14:21:08 +0200

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -3,11 +3,9 @@ import {getFilter} from './filters.js';
 const BASE_URL = 'https://food-boutique.b.goit.study/api/products';
 
 export async function getCategories() {
-	const response = await axios.get(`https://food-boutique.b.goit.study/api/products/categories`);
-	return response.data;
+  const response = await axios.get(`https://food-boutique.b.goit.study/api/products/categories`);
+  return response.data;
 }
-// import { getFilter } from './filters.js';
-const BASE_URL = 'https://food-boutique.b.goit.study/api/products';
 
 export const fetchPopularProducts = () => {
   const params = {
