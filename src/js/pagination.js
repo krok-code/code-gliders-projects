fa42160ed5cede6a08b4800e183e9701dcff873b
Maryna Lykholot <marina.litkovets96@gmail.com>
2023-12-10 15:02:56 +0200

    конструктор Pagination з бібліотеки tui-pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -7,3 +7,36 @@ import { openProductModal } from './card-button.js';
 import { saveToLocalStorage } from './addToCart.js';
 
 
+const productsListGeneral = document.querySelector('.products-list-general');
+const container = document.querySelector('#tui-pagination-container');
+
+
+
+
+const options = {
+  itemsPerPage: 1,
+  visiblePages: 4,
+  page: 1,
+  centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton: `<a href="#" class="icon tui-page-btn tui-{{type}}">
+      <span class="tui-ico-{{type}}">{{type}}>
+      </span>
+      </a>`,
+    disabledMoveButton: `<span class="tui-page-btn tui-is-disabled tui-{{type}}">
+      <span class="tui-ico-{{type}}">{{type}}>
+      </span>
+      </span>`,
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+};
+
+const pagination = new Pagination(container, options);
