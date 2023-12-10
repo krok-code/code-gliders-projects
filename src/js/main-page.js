b9110884ae1375f0775d5624bb313ca3b8af0a76
krok-code <maks.smolinskyi@gmail.com>
2023-12-10 12:40:50 +0200

    Update main-page.js

--- src/js/main-page.js
+++ src/js/main-page.js
@@ -22,3 +22,26 @@ import { openProductModal } from './button-cart.js';
 import { saveToLocalStorage } from './add-to-the-cart.js';
 import localStorageAPI from './local-storage.js';
 import { renderSorryMessage } from './model/sorry.js';
+
+const searchForm = document.querySelector('.filters-form');
+const categoriesInput = document.querySelector('.filters-categories');
+const allSearchInput = document.querySelector('.filters-allTypes');
+const downBtn = document.querySelectorAll('.filters-down-svg');
+const allTypesItem = document.querySelectorAll('.filters-allTypes-item');
+const productsListGeneral = document.querySelector('.products-list-general');
+const productListDiscount = document.querySelector('.products-list-discount');
+const productListPopular = document.querySelector('.products-list-popular');
+
+export let arrProducts = [];
+
+const fillarrProducts = () => {
+  const dataFromLS = localStorageAPI.load('product');
+
+  if (dataFromLS === undefined) {
+    document.querySelector('#header-length').innerHTML = '0';
+    return;
+  }
+  document.querySelector('#header-length').innerHTML = dataFromLS.length;
+  arrProducts = dataFromLS;
+};
+fillarrProducts();
