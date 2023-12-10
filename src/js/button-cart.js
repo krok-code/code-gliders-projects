8e353d9955e0728730ae572b1f0bbde58d8e3172
krok-code <maks.smolinskyi@gmail.com>
2023-12-10 15:09:50 +0200

    Update button-cart.js

--- src/js/button-cart.js
+++ src/js/button-cart.js
@@ -1,3 +1,10 @@
 import { getProducttById } from "./api.js";
 import { onRenderModalProduct, onClickCloseModal, onClickOutModalProduct, onEscapeCloseModal, closeModal, loseModal, addToCart } from "./model/modal-products.js";
 import { addToCardFromModal } from "./main-page.js";
+
+export async function openProductModal(event) {
+	if (event, target.closest('.js-addToCart-btn')) {
+		return;
+	}
+}
+
