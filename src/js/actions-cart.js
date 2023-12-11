c425e487b9a23ea1d1d244b05e725f6054328957
krok-code <maks.smolinskyi@gmail.com>
2023-12-11 02:10:15 +0200

    imports

---
+++ src/js/actions-cart.js
@@ -0,0 +1,7 @@
+import localStorageAPI from "./local-storage.js";
+import iconsPath from '../img/icons.svg';
+import emptyCart from '../images/yellow-shopping-basket.webp';
+import emptyCartRet from '../images/yellow-shopping-basket@2x.webp';
+import emptyCartMob from '../images/yellow-shopping-basket_mob.webp';
+import emptyCartMobRet from '../images/yellow-shopping-basket_mob@2x.webp';
+import emptyCartBase from '../images/yellow-shopping-basket.png';
