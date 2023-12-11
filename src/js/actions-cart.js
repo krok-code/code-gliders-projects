69db4912fc7debfcf1ed0ee3160826b3e3fee4f3
krok-code <maks.smolinskyi@gmail.com>
2023-12-11 02:16:12 +0200

    Update actions-cart.js

--- src/js/actions-cart.js
+++ src/js/actions-cart.js
@@ -1,10 +1,10 @@
 import localStorageAPI from "./local-storage.js";
 import iconsPath from '../img/icons.svg';
-import emptyCart from '../images/yellow-shopping-basket.webp';
-import emptyCartRet from '../images/yellow-shopping-basket@2x.webp';
-import emptyCartMob from '../images/yellow-shopping-basket_mob.webp';
-import emptyCartMobRet from '../images/yellow-shopping-basket_mob@2x.webp';
-import emptyCartBase from '../images/yellow-shopping-basket.png';
+import emptyCart from '../img/yellow-shopping-basket.webp';
+import emptyCartRet from '../img/yellow-shopping-basket@2x.webp';
+import emptyCartMob from '../img/yellow-shopping-basket_mob.webp';
+import emptyCartMobRet from '../img/yellow-shopping-basket_mob@2x.webp';
+import emptyCartBase from '../img/yellow-shopping-basket.png';
 
 import { getLength } from "./header.js";
 import { order } from "./api.js";
