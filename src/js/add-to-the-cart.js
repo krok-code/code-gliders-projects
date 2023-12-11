3bb98177f63bb620d5ffd7c7c4b5d713b06234af
krok-code <maks.smolinskyi@gmail.com>
2023-12-11 12:52:24 +0200

    Update add-to-the-cart.js

--- src/js/add-to-the-cart.js
+++ src/js/add-to-the-cart.js
@@ -1,5 +1,5 @@
 import { getProducttById } from "./api.js";
-import iconsPath from './img/icons.svg';
+import iconsPath from '../img/icons.svg';
 import { getLength } from "./header.js";
 import { arrProducts } from "./main-page.js";
 import localStorageAPI from './local-storage.js';
