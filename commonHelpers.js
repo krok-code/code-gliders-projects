9f874a70ada6da4add2cf8cab8d2211a89e6a505
krok-code <127609706+krok-code@users.noreply.github.com>
2023-12-11 00:16:27 +0000

    Deploying to gh-pages from @ krok-code/code-gliders-projects@69db4912fc7debfcf1ed0ee3160826b3e3fee4f3 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import{l as r}from"./assets/header-62a80ab8.js";function t(){const e=r.load("product");document.querySelector(".cart-number-purchases").innerHTML=`${e?e.length:"0"} `}t();const o=r.load("product");document.addEventListener("DOMContentLoaded",e=>{o.length===0&&(document.querySelector(".section-cart").innerHTML=renderCartEmpty()),addScroll()});
+//# sourceMappingURL=commonHelpers.js.map
