097fc01808bc9481f6458a6925f9dfa522d295ce
krok-code <maks.smolinskyi@gmail.com>
2023-12-12 14:54:40 +0200

    Update button-cart.js

--- src/js/button-cart.js
+++ src/js/button-cart.js
@@ -1,23 +1,30 @@
 import { getProducttById } from './api.js';
-import { onRenderModalProduct, onClickCloseModal, onClickOutModalProduct, onEscapeCloseModal, closeModal, addToCartTheProduct } from './model/modal-products.js';
-import { addToCardFromModal } from './homePage.js';
+import {
+  onRenderModalProduct,
+  onClickCloseModal,
+  onClickOutModalProduct,
+  onEscapeCloseModal,
+} from './model/modal-product.js';
+import { addToCartFromModal } from './homePage.js';
 
 export async function openProductModal(event) {
+  if (event.target.closest('.js-addToCart-btn')) {
+    return;
+  }
+  const ParentElement = event.target.closest('li');
+  const cardButton = ParentElement.querySelector('.js-addToCart-btn');
+  const productId = cardButton.getAttribute('data-id');
+  const productInfo = await getProducttById(productId);
 
-	if (event.target.closest('.js-addToCart-btn')) {
-		return;
-	}
-
-	const ParentElement = event.target.closest('li');
-	const cardButton = ParentElement.querySelector('.js-addToCart-btn');
-	const productId = cardButton.getAttribute('data-id');
-	const productInfo = await getProducttById(productId);
-
-	document.querySelector('body').insertAdjacentHTML('beforeend', onRenderModalProduct(productInfo));
-	document.body.classList.add('is-overflow-hidden');
-	const closeModalButton = document.querySelector('.modal-btn-close');
-	closeModalButton.addEventListener('click', onClickCloseModal);
-	document.addEventListener('keydown', onEscapeCloseModal);
-	document.addEventListener('click', onClickOutModalProduct);
-	document.querySelector('.modal-product-btn-price').addEventListener('click', addToCardFromModal);
+  document
+    .querySelector('body')
+    .insertAdjacentHTML('beforeend', onRenderModalProduct(productInfo));
+  document.body.classList.add('is-overflow-hidden');
+  const closeModalButton = document.querySelector('.modal-btn-close');
+  closeModalButton.addEventListener('click', onClickCloseModal);
+  document.addEventListener('keydown', onEscapeCloseModal);
+  document.addEventListener('click', onClickOutModalProduct);
+  document
+    .querySelector('.modal-product-btn-price')
+    .addEventListener('click', addToCartFromModal);
 }
