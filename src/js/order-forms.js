2f77e98704ce2ca66f20205ca7aa8eb234d32f43
krok-code <maks.smolinskyi@gmail.com>
2023-12-12 14:58:15 +0200

    Update order-forms.js

--- src/js/order-forms.js
+++ src/js/order-forms.js
@@ -1,39 +1,31 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-import { order } from './api.js';
+
 import localStorageAPI from './localStorage.js';
 
-const input = document.querySelector('.datatime');
+const input = document.querySelector('.datetime');
 
 const options = {
-	enableTime: true,
-	time_24hr: true,
-	defaultDate: new Date(),
-	minuteIncrement: 1,
-}
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+};
 
 flatpickr(input, options);
 
 document.addEventListener('DOMContentLoaded', function () {
-	const orderButton = document.querySelector('.cart-order-info-btn');
+  const orderButton = document.querySelector('.cart-order-info-btn');
 
-	orderButton.addEventListener('submit', function (event) {
-		event.preventDefault();
+  orderButton.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const mail = document.getElementById('mail').value;
 
-		const name = document.getElementById('name').value;
-		const phone = document.getElementById('phone').value;
-		const address = document.getElementById('address').value;
-		const datetime = document.getElementById('datetime').value;
+    const order = {
+      mail,
+    };
 
-		const order = {
-			name,
-			phone,
-			address,
-			datetime,
-		};
-
-		console.log(error);
-		localStorageAPI.save('lastOrder', order);
-	});
-
+    console.log(order);
+    localStorageAPI.save('lastOrder', order);
+  });
 });
