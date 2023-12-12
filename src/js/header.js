0504ebe87f5204ea24b6cf85b97abdd27a258fb2
krok-code <maks.smolinskyi@gmail.com>
2023-12-12 14:47:32 +0200

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,7 @@
 import localStorageAPI from './localStorage.js';
 
 export function getLength() {
-	const arrFromLS = localStorageAPI.load('product');
-	document.querySelector('#header-length').innerHTML = `${arrFromLS ? arrFromLS.length : '0'}`;
+    const arrFromLS = localStorageAPI.load('product');
+    document.querySelector('#header-length').innerHTML = `${arrFromLS ? arrFromLS.length : '0'}`;
 }
 getLength();
