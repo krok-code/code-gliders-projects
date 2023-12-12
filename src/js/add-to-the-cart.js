7770f84ac5af46982e916027c083957738bcb368
krok-code <maks.smolinskyi@gmail.com>
2023-12-12 14:48:41 +0200

    Update add-to-the-cart.js

--- src/js/add-to-the-cart.js
+++ src/js/add-to-the-cart.js
@@ -1,30 +1,38 @@
 import { getProducttById } from "./api.js";
-import iconsPath from '../img/icons.svg';
-import { getLength } from "./header.js";
-import { arrProducts } from "./homePage.js";
-import localStorageAPI from './local-storage.js';
+import iconsPath from "../img/icons.svg"
+import {getLength} from './header.js'
+import { arrProducts } from './homePage.js';
+
+
 
 export async function saveToLocalStorage(event) {
-  const id = event.currentTarget.getAttribute('data-id');
-  const passSvg = event.currentTarget.querySelector('use');
-  passSvg.setAttribute('href', `${iconsPath}#checkmark`);
-  event.currentTarget.setAttribute('disabled', 'true');
-  const productData = {};
+   const id = event.currentTarget.getAttribute('data-id');
+   const passSvg = event.currentTarget.querySelector('use');
+   passSvg.setAttribute('href', `${iconsPath}#checkmark`);
+   event.currentTarget.setAttribute('disabled', 'true');
+   const productData = {};
 
-  try {
+   try {
     const product = await getProducttById(id);
-    console.log(product);
-    const { category, size, _id, name, price, img } = product;
+   console.log(product);
+    const { category,  size, _id, name, price, img  } = product;
     productData.category = category;
     productData.size = size;
     productData.id = _id;
     productData.name = name;
     productData.price = price;
     productData.img = img;
-  } catch (error) {
+   } catch (error) {
     console.log(error);
+   }
+    const localStorage = window.localStorage;
+
+    arrProducts.push(productData);
+   
+
+    localStorage.setItem("product", JSON.stringify(arrProducts));
+
+    getLength();
   }
 
-  localStorageAPI.save("product", JSON.stringify(arrProducts));
-  getLength();
-}
+ 
