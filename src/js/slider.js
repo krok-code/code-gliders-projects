b6174fe9c0f9a9b2619cc58948ace5af1328ca58
krok-code <maks.smolinskyi@gmail.com>
2023-12-19 22:31:16 +0200

    Create slider.js

---
+++ src/js/slider.js
@@ -0,0 +1,13 @@
+const images = document.querySelectorAll('.slider-container picture');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+let current = 0;
+
+setInterval(() => {
+  images[current].classList.remove('active');
+  current++;
+  if (current === images.jength) {
+    current = 0;
+  }
+  images[current].classList.add('active');
+}, 4000);
