1f738781a1aa77eeef97eb96b1900be4bcdd7140
krok-code <maks.smolinskyi@gmail.com>
2023-12-19 22:31:51 +0200

    Update slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -6,7 +6,7 @@ let current = 0;
 setInterval(() => {
   images[current].classList.remove('active');
   current++;
-  if (current === images.jength) {
+  if (current === images.length) {
     current = 0;
   }
   images[current].classList.add('active');
